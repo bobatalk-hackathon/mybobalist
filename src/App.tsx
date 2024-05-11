47e660dc08d1c10176693cadf78f008988ac6337
nova_44056 <senguy44056@gmail.com>
2024-05-11 13:39:34 -0500

    add example on how to fetch from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/api/boba/top-shops/united-states"
+        ); // Adjust URL as necessary
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setData(null);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Boba Shops</h1>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+    </div>
+  );
+};
 
 export default App;
